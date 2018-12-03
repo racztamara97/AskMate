9cb8c0aa02ac2de7186caac5dcabb924f6bbd808
racztamara97 <racztamara97@gmail.com>
2018-12-03 14:18:17 +0100

    service annotation added

--- src/main/java/com/code/cool/askmate/askmate/service/LoginService.java
+++ src/main/java/com/code/cool/askmate/askmate/service/LoginService.java
@@ -2,7 +2,9 @@ package com.code.cool.askmate.askmate.service;
 
 import com.code.cool.askmate.askmate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginService {
 
     @Autowired
