c6c506ca8e5496b2e21bac698b1d3cfaac6b598f
racztamara97 <racztamara97@gmail.com>
2018-12-03 14:19:37 +0100

    getUserByName and getUserByPassword methods added

--- src/main/java/com/code/cool/askmate/askmate/repository/UserRepository.java
+++ src/main/java/com/code/cool/askmate/askmate/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String>{
 
-    }
+    User getUserByUsername(String username);
+
+    User getUserByPassword(String password);
+}
