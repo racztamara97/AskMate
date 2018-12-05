74136a2a9e6ab804f656f02e2d5a7f100dadda13
racztamara97 <racztamara97@gmail.com>
2018-12-05 13:34:20 +0100

    int id changed to long and toString method is overridden

--- src/main/java/com/code/cool/askmate/askmate/model/User.java
+++ src/main/java/com/code/cool/askmate/askmate/model/User.java
@@ -10,7 +10,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
     private String name;
     private String username;
     private String password;
@@ -24,11 +24,11 @@ public class User {
         this.password = password;
     }
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -55,4 +55,15 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
 }
