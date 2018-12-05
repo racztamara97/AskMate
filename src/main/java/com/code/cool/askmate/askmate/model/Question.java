d2840c191bdb6baeabf3ac605d643b0ddb7b0577
racztamara97 <racztamara97@gmail.com>
2018-12-05 13:33:34 +0100

    int id changed to long, int userId changed to User user, toString method overridden

--- src/main/java/com/code/cool/askmate/askmate/model/Question.java
+++ src/main/java/com/code/cool/askmate/askmate/model/Question.java
@@ -7,36 +7,25 @@ public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id;
-    private int user_id;
+    public long id;
+    private User user;
     private String question_title;
     private String question_description;
-    private int vote_number;
+    private int vote_number = 0;
 
-    public Question() {
-    }
-
-    public Question(int user_id, String question_name, String question_description, int vote_number) {
-        this.user_id = user_id;
-        this.question_title = question_name;
-        this.question_description = question_description;
-        this.vote_number = vote_number;
-    }
-
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
-
-    public int getUser_id() {
-        return user_id;
+    public User getUser() {
+        return user;
     }
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public String getQuestion_title() {
@@ -63,4 +52,14 @@ public class Question {
         this.vote_number = vote_number;
     }
 
+   @Override
+    public String toString(){
+       return "Question{" +
+               "id=" + id +
+               ", user=" + user +
+               ", question_title=" + question_title +
+               ", question1_description=" + question_description +
+               ", vote_number=" + vote_number +
+               '}';
+   }
 }
