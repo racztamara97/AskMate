b2b3504129252ba23e5a23be1194ac19f59b6194
racztamara97 <racztamara97@gmail.com>
2018-12-06 13:24:05 +0100

    findAll added

--- src/main/java/com/code/cool/askmate/askmate/repository/QuestionRepository.java
+++ src/main/java/com/code/cool/askmate/askmate/repository/QuestionRepository.java
@@ -3,6 +3,11 @@ package com.code.cool.askmate.askmate.repository;
 import com.code.cool.askmate.askmate.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface QuestionRepository extends JpaRepository<Question, String>{
+
+    @Override
+    List<Question> findAll();
 }
