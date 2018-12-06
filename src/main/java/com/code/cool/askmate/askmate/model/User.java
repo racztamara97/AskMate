1d953bf30746e55910c21e1b9e79aa9d6cbc3b45
racztamara97 <racztamara97@gmail.com>
2018-12-06 10:52:00 +0100

    Question's list added to user

--- src/main/java/com/code/cool/askmate/askmate/model/User.java
+++ src/main/java/com/code/cool/askmate/askmate/model/User.java
@@ -1,9 +1,8 @@
 package com.code.cool.askmate.askmate.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "Users")
 public class User {
@@ -14,6 +13,8 @@ public class User {
     private String name;
     private String username;
     private String password;
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Question> questions = new ArrayList<>();
 
     public User() {
     }
@@ -54,6 +55,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
     }
 
     @Override
