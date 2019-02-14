9c4fa4dc24e11d24c939565f10a61b4a7f3fb5fe
racztamara97 <racztamara97@gmail.com>
2019-02-14 14:10:23 +0100

    Vote model initialized

---
+++ src/main/java/com/code/cool/askmate/askmate/model/Vote.java
@@ -0,0 +1,45 @@
+package com.code.cool.askmate.askmate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Votes")
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long userId;
+    private long questionId;
+
+    public Vote(long userId, long questionId) {
+        this.userId = userId;
+        this.questionId = questionId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+}
