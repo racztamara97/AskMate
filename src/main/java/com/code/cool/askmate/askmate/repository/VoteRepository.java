e1efeaeabc04c75a3b98219e01f52bd2e2435e18
racztamara97 <racztamara97@gmail.com>
2019-02-15 08:57:47 +0100

    findVoteByUserId

--- src/main/java/com/code/cool/askmate/askmate/repository/VoteRepository.java
+++ src/main/java/com/code/cool/askmate/askmate/repository/VoteRepository.java
@@ -3,5 +3,11 @@ package com.code.cool.askmate.askmate.repository;
 import com.code.cool.askmate.askmate.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VoteRepository extends JpaRepository<Vote, String> {
+
+    public List<Vote> findVotesByUserId(long userId);
+
+    public Vote findVoteById(long id);
 }
