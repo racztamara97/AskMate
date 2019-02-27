15ce88d7d1ec2ccf1d06d8b39126488645bb89f4
racztamara97 <racztamara97@gmail.com>
2019-02-27 09:51:24 +0100

    CheckVoteExists refactor because of the VoteType

--- src/main/java/com/code/cool/askmate/askmate/service/VoteService.java
+++ src/main/java/com/code/cool/askmate/askmate/service/VoteService.java
@@ -1,5 +1,6 @@
 package com.code.cool.askmate.askmate.service;
 
+import com.code.cool.askmate.askmate.VoteType;
 import com.code.cool.askmate.askmate.model.Question;
 import com.code.cool.askmate.askmate.model.Vote;
 import com.code.cool.askmate.askmate.repository.QuestionRepository;
@@ -19,44 +20,55 @@ public class VoteService {
     @Autowired
     QuestionRepository questionRepository;
 
-    public void createNewVote(long userId, long questionId) {
+    public void createNewVote(long userId, long questionId, VoteType type) {
 
         Vote newVote = new Vote();
         newVote.setUserId(userId);
         newVote.setQuestionId(questionId);
+        newVote.setType(type);
         voteRepository.save(newVote);
     }
 
-
-    public boolean checkVoteExsits(long userId, long questionId) {
+    public boolean checkVoteExists(long questionId, long userId, VoteType type) {
         List<Vote> votesOfThisUser = voteRepository.findVotesByUserId(userId);
         if (votesOfThisUser != null) {
             for (Vote actual : votesOfThisUser) {
                 if (actual.getQuestionId() == questionId) {
+                    if(actual.getType()!=type){
+                        if(actual.getType()==VoteType.UP){
+                            voteDown(questionId,userId);
+                        }
+                        else {
+                            voteUp(questionId,userId);
+                        }
+                        voteRepository.delete(actual);
+                        Vote delete = voteRepository.findVoteByUserIdAndQuestionId(userId, questionId);
+                        voteRepository.delete(delete);
+                        return false;
+
+                    }
                     return true;
                 }
             }
-            //createNewVote(userId, questionId);
-            //voteUp(questionId);
             return false;
         }
-        //createNewVote(userId, questionId);
-        //voteUp(questionId);
         return false;
     }
 
-    public void voteUp(long questionId) {
+    public void voteUp(long questionId, long userId) {
         Question actual = questionRepository.getQuestionById(questionId);
         int voteBefore = actual.getVoteNumber();
         actual.setVoteNumber(voteBefore + 1);
+        createNewVote(userId, questionId, VoteType.UP);
         questionRepository.save(actual);
     }
 
-    public void voteDown(long questionId) {
+    public void voteDown(long questionId, long userId) {
         Question actual = questionRepository.getQuestionById(questionId);
         int voteBefore = actual.getVoteNumber();
         if (voteBefore != 0) {
             actual.setVoteNumber(voteBefore - 1);
+            createNewVote(userId, questionId, VoteType.DOWN);
             questionRepository.save(actual);
         }
     }
