5d3c44b44a2f930c5a69aa8afc462c0f7e1d456f
racztamara97 <racztamara97@gmail.com>
2019-03-14 09:47:15 +0100

    refactor

--- src/main/java/com/code/cool/askmate/askmate/service/CommentService.java
+++ src/main/java/com/code/cool/askmate/askmate/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.code.cool.askmate.askmate.service;
 
 import com.code.cool.askmate.askmate.model.Comment;
+import com.code.cool.askmate.askmate.model.User;
 import com.code.cool.askmate.askmate.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,9 @@ public class CommentService {
     @Autowired
     CommentRepository commentRepository;
 
-    public void createNewComment(long userId, long questionId, Comment comment) {
+    public void createNewComment(User user, long questionId, Comment comment) {
         Comment newComment = new Comment();
-        newComment.setUserId(userId);
+        newComment.setUser(user);
         newComment.setQuestionId(questionId);
         newComment.setCommentText(comment.getCommentText());
         newComment.setVoteNumber(comment.getVoteNumber());
